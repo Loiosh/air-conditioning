52b9156d23c97d2c35e03e7746c704267cc06590
Loiosh <robin.delmas@gmail.com>
2025-06-19 13:48:15 +0000

    NO

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,11 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+st.title("Is the air conditioning working?")
+st.markdown(
+    """ 
+    ## NO 
+    """
 )
+
+if st.button("Send help!"):
+    st.balloons()
